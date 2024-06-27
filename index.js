348b8300d4a99a349c4e48fb81cd4b3525f3065b
Arham Afzal <wwwarhamafzal682@gmail.com>
2024-06-28 04:50:42 +0500

    Word Counter

---
+++ index.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const output = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count words:",
+    },
+]);
+const words = output.sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length} words`);
